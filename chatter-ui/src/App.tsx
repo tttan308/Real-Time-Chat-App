18982a0454f9dac0e03a1e66f63270571a2a2eea
21120553 <tantran.300803@gmail.com>
2024-04-18 00:16:16 +0700

    initialize

---
+++ chatter-ui/src/App.tsx
@@ -0,0 +1,27 @@
+import {
+  Container,
+  CssBaseline,
+  ThemeProvider,
+  createTheme,
+} from "@mui/material";
+import { RouterProvider } from "react-router-dom";
+import router from "./components/Route";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+function App() {
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Container>
+        <RouterProvider router={router} />
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default App;
